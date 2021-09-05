5093d279c71ce6805593b394c28f527ac3ae3bd2
Skipper <Dima29koz@yandex.ru>
2021-09-05 17:49:54 +0300

    basic_calc v1.0

--- GUI/basic_calc.py
+++ GUI/basic_calc.py
@@ -8,6 +8,8 @@ class Calc:
     def __init__(self, canvas):
         self._activeStr = ''
         self._gui_calc(canvas)
+        self.label_input = Label(canvas, text='0', width=50)
+        self.label_input.grid(row=0, column=0, columnspan=4, rowspan=2)
 
     def _gui_calc(self, canvas):
         buttons = (
@@ -18,46 +20,86 @@ class Calc:
             ('switch', '0', '.', '=')
         )
         stack = []
-        label = Label(canvas, text='0', width=50)
-        label.grid(row=0, column=0, columnspan=4, rowspan=2)
 
         for row_idx, row in enumerate(buttons):
             for col_idx, btn in enumerate(row):
                 button = Button(canvas, text=btn,
-                                command=partial(self._click, btn, label, stack))
+                                command=partial(self._click, btn, stack))
                 button.grid(row=row_idx + 3, column=col_idx, sticky="nsew")
 
-    def _click(self, text, label: Label, stack):
-        # btn_text_to_action = {}
+    def _click(self, text, stack: list):
+        if text == 'switch':
+            pass  # todo
         if text == 'AC':
             stack.clear()
             self._activeStr = ''
-            label.configure(text='0')
         elif text == '<-':
-            if len(self._activeStr) > 1:
+            if len(self._activeStr) > 0:
                 self._activeStr = self._activeStr[:-1]
-                label.configure(text=self._activeStr)
             else:
-                label.configure(text='0')
+                if len(stack) > 0:
+                    self._activeStr = stack.pop()
         elif '0' <= text <= '9':
+            if self._activeStr in ['/', '*', '-', '+']:
+                stack.append(self._activeStr)
+                self._activeStr = ''
             self._activeStr += text
-            label.configure(text=self._activeStr)
         elif text == '.':
+            if self._activeStr in ['/', '*', '-', '+']:
+                stack.append(self._activeStr)
+                self._activeStr = ''
             if self._activeStr.find('.') == -1:
                 self._activeStr += text
-                label.configure(text=self._activeStr)
-        else:
-            if len(stack) >= 2:
-                stack.append(label['text'])
-                label.configure(text=str(calculate(stack)))
-                stack.clear()
-                stack.append(label['text'])
-                self._activeStr = ''
-                if text != '=':
-                    stack.append(text)
+        elif text == '%':
+            self._activeStr = str(calculate([self._activeStr, '/', '100']))
+        elif text in ['/', '*', '-', '+']:
+            if self._activeStr:
+                if self._activeStr not in ['/', '*', '-', '+']:
+                    stack.append(self._activeStr)
+                    self._activeStr = text
+                else:
+                    self._activeStr = text
             else:
-                if text != '=':
-                    stack.append(label['text'])
-                    stack.append(text)
-                    self._activeStr = ''
-                    # label.configure(text='0')
+                if text == '-':
+                    self._activeStr = text
+        elif text == '=':
+            stack.append(self._activeStr)
+            self._activeStr = calculate(stack)
+            stack.clear()
+
+        input_str = ''.join(stack) + self._activeStr
+        self.label_input.configure(text=input_str if input_str else '0')
+
+        # # btn_text_to_action = {}
+        # if text == 'AC':
+        #     stack.clear()
+        #     self._activeStr = ''
+        #     self.label_input.configure(text='0')
+        # elif text == '<-':
+        #     if len(self._activeStr) > 1:
+        #         self._activeStr = self._activeStr[:-1]
+        #         self.label_input.configure(text=self._activeStr)
+        #     else:
+        #         self.label_input.configure(text='0')
+        # elif '0' <= text <= '9':
+        #     self._activeStr += text
+        #     self.label_input.configure(text=self._activeStr)
+        # elif text == '.':
+        #     if self._activeStr.find('.') == -1:
+        #         self._activeStr += text
+        #         self.label_input.configure(text=self._activeStr)
+        # else:
+        #     if len(stack) >= 2:
+        #         stack.append(self._activeStr)
+        #         self.label_input.configure(text=str(calculate(stack)))
+        #         stack.clear()
+        #         stack.append(self.label_input['text'])
+        #         self._activeStr = ''
+        #         if text != '=':
+        #             stack.append(text)
+        #     else:
+        #         if text != '=':
+        #             stack.append(self.label_input['text'])
+        #             stack.append(text)
+        #             self._activeStr = ''
+        #             # label.configure(text='0')
--- basic_calculator/calcs.py
+++ basic_calculator/calcs.py
@@ -1,12 +1,23 @@
 from decimal import Decimal
 
 
-def calculate(stack):
-    result = 0
-    operand2 = Decimal(stack.pop())
-    operation = stack.pop()
-    operand1 = Decimal(stack.pop())
+def calculate(stack: list):
+    operators = ['*', '/', '+', '-']
+    for operator in operators:
+        while operator in stack:
+            stack = _calc(stack, operator)
+    return stack[0]
 
+
+def _calc(stack: list, current_operator: str):
+    operation_idx = stack.index(current_operator)
+
+    operand1 = Decimal(stack[operation_idx - 1])
+    operand2 = Decimal(stack[operation_idx + 1])
+    operation = current_operator
+    result = 0
+    for _ in range(3):
+        stack.pop(operation_idx - 1)
     if operation == '+':
         result = operand1 + operand2
     if operation == '-':
@@ -15,4 +26,6 @@ def calculate(stack):
         result = operand1 / operand2
     if operation == '*':
         result = operand1 * operand2
-    return result
+
+    stack.insert(operation_idx - 1, str(result))
+    return stack
