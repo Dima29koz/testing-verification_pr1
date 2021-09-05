f9f43629e14f88de69fc1e71aff1cbd5501a0df0
Skipper <Dima29koz@yandex.ru>
2021-09-05 18:22:14 +0300

    basic_calc v1.1 исправлены ошибки: вылет при делении на 0 добавлено: динамическое вычисление результата

--- GUI/basic_calc.py
+++ GUI/basic_calc.py
@@ -1,3 +1,4 @@
+from copy import copy
 from tkinter import *
 from functools import partial
 
@@ -7,9 +8,12 @@ from basic_calculator.calcs import calculate
 class Calc:
     def __init__(self, canvas):
         self._activeStr = ''
+        self.stack = []
         self._gui_calc(canvas)
         self.label_input = Label(canvas, text='0', width=50)
-        self.label_input.grid(row=0, column=0, columnspan=4, rowspan=2)
+        self.label_input.grid(row=0, column=0, columnspan=4)
+        self.label_result = Label(canvas, text='=', width=50, anchor='e')
+        self.label_result.grid(row=2, column=0, columnspan=4)
 
     def _gui_calc(self, canvas):
         buttons = (
@@ -19,34 +23,34 @@ class Calc:
             ('1', '2', '3', '+'),
             ('switch', '0', '.', '=')
         )
-        stack = []
 
         for row_idx, row in enumerate(buttons):
             for col_idx, btn in enumerate(row):
                 button = Button(canvas, text=btn,
-                                command=partial(self._click, btn, stack))
-                button.grid(row=row_idx + 3, column=col_idx, sticky="nsew")
+                                command=partial(self._click, btn))
+                button.grid(row=row_idx + 4, column=col_idx, sticky="nsew")
 
-    def _click(self, text, stack: list):
+    def _click(self, text):
         if text == 'switch':
             pass  # todo
         if text == 'AC':
-            stack.clear()
+            self.stack.clear()
             self._activeStr = ''
         elif text == '<-':
             if len(self._activeStr) > 0:
                 self._activeStr = self._activeStr[:-1]
             else:
-                if len(stack) > 0:
-                    self._activeStr = stack.pop()
+                if len(self.stack) > 0:
+                    self._activeStr = self.stack.pop()
+                    self._activeStr = self._activeStr[:-1]
         elif '0' <= text <= '9':
             if self._activeStr in ['/', '*', '-', '+']:
-                stack.append(self._activeStr)
+                self.stack.append(self._activeStr)
                 self._activeStr = ''
             self._activeStr += text
         elif text == '.':
             if self._activeStr in ['/', '*', '-', '+']:
-                stack.append(self._activeStr)
+                self.stack.append(self._activeStr)
                 self._activeStr = ''
             if self._activeStr.find('.') == -1:
                 self._activeStr += text
@@ -55,7 +59,7 @@ class Calc:
         elif text in ['/', '*', '-', '+']:
             if self._activeStr:
                 if self._activeStr not in ['/', '*', '-', '+']:
-                    stack.append(self._activeStr)
+                    self.stack.append(self._activeStr)
                     self._activeStr = text
                 else:
                     self._activeStr = text
@@ -63,43 +67,21 @@ class Calc:
                 if text == '-':
                     self._activeStr = text
         elif text == '=':
-            stack.append(self._activeStr)
-            self._activeStr = calculate(stack)
-            stack.clear()
+            self.stack.append(self._activeStr)
+            self._activeStr = calculate(self.stack)
+            self.stack.clear()
 
-        input_str = ''.join(stack) + self._activeStr
+        input_str = ''.join(self.stack) + self._activeStr
         self.label_input.configure(text=input_str if input_str else '0')
 
-        # # btn_text_to_action = {}
-        # if text == 'AC':
-        #     stack.clear()
-        #     self._activeStr = ''
-        #     self.label_input.configure(text='0')
-        # elif text == '<-':
-        #     if len(self._activeStr) > 1:
-        #         self._activeStr = self._activeStr[:-1]
-        #         self.label_input.configure(text=self._activeStr)
-        #     else:
-        #         self.label_input.configure(text='0')
-        # elif '0' <= text <= '9':
-        #     self._activeStr += text
-        #     self.label_input.configure(text=self._activeStr)
-        # elif text == '.':
-        #     if self._activeStr.find('.') == -1:
-        #         self._activeStr += text
-        #         self.label_input.configure(text=self._activeStr)
-        # else:
-        #     if len(stack) >= 2:
-        #         stack.append(self._activeStr)
-        #         self.label_input.configure(text=str(calculate(stack)))
-        #         stack.clear()
-        #         stack.append(self.label_input['text'])
-        #         self._activeStr = ''
-        #         if text != '=':
-        #             stack.append(text)
-        #     else:
-        #         if text != '=':
-        #             stack.append(self.label_input['text'])
-        #             stack.append(text)
-        #             self._activeStr = ''
-        #             # label.configure(text='0')
+        tmp_stack = copy(self.stack)
+        if self._activeStr:
+            tmp_stack.append(self._activeStr)
+        if tmp_stack:
+            result_str = calculate(tmp_stack)
+        else:
+            result_str = ''
+        if result_str.startswith('#'):
+            self.label_result.configure(text=result_str)
+        else:
+            self.label_result.configure(text='=' + result_str if result_str else '=')
